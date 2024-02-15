633db5d05064333cd66e18d0ac680a16186344de
Washiq Shamsi <jakas1gy@gmail.com>
2024-02-15 22:06:56 +0600

    Extension Method Implement

---
+++ KhatiExtendedEF/Extensions/KhatiExtensions.cs
@@ -0,0 +1,24 @@
+﻿using KhatiExtendedEF.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace KhatiExtendedEF.Extensions
+{
+    public static class KhatiExtensions
+    {
+        public static async Task<PaginationResponseModel<T>> PaginationAsync<T>(this IQueryable<T> list, int pageSize = 10, int pageIndex = 1) where T : class
+        {
+            var model = new PaginationResponseModel<T>()
+            {
+                PageIndex = pageIndex,
+                PageSize = pageSize,
+                TotalData = await list.CountAsync(),
+                Data = await list.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync(),
+            };
+
+            model.HasNextPage = Math.Ceiling(Convert.ToDecimal(model.TotalData) / Convert.ToDecimal(pageSize)) > model.PageIndex;
+            model.HasPreviousPage = model.TotalData > pageSize && model.PageIndex > 1;
+
+            return model;
+        }
+    }
+}
